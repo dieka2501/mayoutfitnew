2f8f6f547737299b66381d54d78ea366f10cbf0c
dikdik <dieka.koes@gmail.com>
2016-12-22 00:30:41 +0700

    analitik

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -17,6 +17,16 @@
 <script src="{{Config::get('app.url')}}assets/front/js/bootstrap.min.js"></script>
 <script src="{{Config::get('app.url')}}assets/front/js/scripts.js"></script>
 <script src="{{Config::get('app.url')}}assets/front/js/owl.carousel.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71427144-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
