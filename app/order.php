7da74e3a09fe6c92bc9a7cbedd843808ea7185fd
dikdik <dieka.koes@gmail.com>
2017-01-05 21:47:41 +0700

    Report order

--- app/order.php
+++ app/order.php
@@ -65,13 +65,17 @@ class order extends Model
 
     //report
     function get_page_report(){
-		return order::join('order_detail',$this->table.'.idorder','=','order_detail.order_id')
+		return order::select(DB::raw('product.product_name, sum(order_detail.order_detail_qty) as order_detail_qty, sum(order_detail.order_detail_price) as order_detail_price,sum(order_detail.order_detail_discount_nominal) as order_detail_discount_nominal,product.product_hpp as product_hpp '))
+						->join('order_detail',$this->table.'.idorder','=','order_detail.order_id')
 						->join('product','order_detail.product_id','=','product.idproduct')
-						->orderBy('product.product_name','asc')->paginate(20);
+						->orderBy('product.product_name','asc')
+						->groupBy('product.product_name')
+						->paginate(20);
 	}
 
 	function get_search_report($date_start,$date_end){
-		return order::join('order_detail',$this->table.'.idorder','=','order_detail.order_id')
+		return order::select(DB::raw('product.product_name, sum(order_detail.order_detail_qty) as order_detail_qty, sum(order_detail.order_detail_price) as order_detail_price,sum(order_detail.order_detail_discount_nominal) as order_detail_discount_nominal,product.product_hpp as product_hpp '))
+						->join('order_detail',$this->table.'.idorder','=','order_detail.order_id')
 						->join('product','order_detail.product_id','=','product.idproduct')
 						->whereBetween($this->table.'.created_at',[$date_start.'%',$date_end."%"])
 						->orderBy('product.product_name','asc')->paginate(20);
